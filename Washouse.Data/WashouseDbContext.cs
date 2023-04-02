192042585db72f121b434b5562700a5ad170eda4
datlt2001 <84031043+datlt2001@users.noreply.github.com>
2023-04-02 15:24:53 +0700

    Update WashouseDbContext.cs

--- Washouse.Data/WashouseDbContext.cs
+++ Washouse.Data/WashouseDbContext.cs
@@ -1250,14 +1250,14 @@ namespace Washouse.Data
                        .AddJsonFile("appsettings.json")
                        .Build();
                 }*/
+//                 configuration = new ConfigurationBuilder()
+//                        .SetBasePath(Directory.GetParent(Directory.GetCurrentDirectory()).ToString() + "\\Washouse.Web")
+//                        .AddJsonFile("appsettings.json")
+//                        .Build();
                 configuration = new ConfigurationBuilder()
-                       .SetBasePath(Directory.GetParent(Directory.GetCurrentDirectory()).ToString() + "\\Washouse.Web")
-                       .AddJsonFile("appsettings.json")
-                       .Build();
-                /*configuration = new ConfigurationBuilder()
                                    .SetBasePath(Directory.GetCurrentDirectory())
                                    .AddJsonFile("appsettings.json")
-                                   .Build();*/
+                                   .Build();
                 var connectionString = configuration.GetConnectionString("WashouseDB");
                 //var connectionString = "Server=washouse.database.windows.net;Uid=washouseAdmin;Pwd=Washouse123!;Database= WashouseDb ";
                 optionsBuilder.UseSqlServer(connectionString);
