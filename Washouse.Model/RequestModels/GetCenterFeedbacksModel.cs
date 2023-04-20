8e6a5c26b163679c38534b90ff15687509c1ea28
nesto2001 <tanlong6121@gmail.com>
2023-04-20 12:35:23 +0700

    get my-center/feedbacks

---
+++ Washouse.Model/RequestModels/GetCenterFeedbacksModel.cs
@@ -0,0 +1,10 @@
+﻿using Washouse.Model.ViewModel;
+
+namespace Washouse.Model.RequestModels
+{
+    public class GetCenterFeedbacksModel : PaginationViewModel
+    {
+        public int? ServiceId { get; set; }
+        public string? OrderId { get; set; }
+    }
+}
