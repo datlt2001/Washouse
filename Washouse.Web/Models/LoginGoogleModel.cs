4f40120669178edacb3906a52ae89afaf67fee38
nesto2001 <tanlong6121@gmail.com>
2023-04-23 02:20:25 +0700

    login google

---
+++ Washouse.Web/Models/LoginGoogleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Washouse.Web.Models
+{
+    public class LoginGoogleModel
+    {
+        [Required] public string Code { get; set; }
+        [Required] public string RedirectUri { get; set; }
+    }
+}
